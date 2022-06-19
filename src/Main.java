a6442e6f339b881074ea22a2c70509c3da0b25c1
“Daria <dariapilevich@gmail.com>
2022-06-19 20:03:01 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10012;
+        int mileCost = 20;
+        int bonusMiles = ticketPrice / mileCost;
+        System.out.println(bonusMiles);
+    }
+}
